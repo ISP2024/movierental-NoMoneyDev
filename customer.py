466c7a78021285b863f790b37a734584d5c1c454
Rattanan RUNG-UTHAI <japanrisesgamer@gmail.com>
2024-10-14 16:20:39 +0700

    Extract Method to compute total charge

--- customer.py
+++ customer.py
@@ -33,7 +33,7 @@ class Customer:
         Returns:
             the statement as a String
         """
-        total_amount = 0   # total rental charges
+        total_amount = self.total_amount()  # total rental charges
         frequent_renter_points = 0
         # the .format method substitutes actual values into the fmt string
         statement = f"Rental Report for {self.name}\n\n"
@@ -43,20 +43,12 @@ class Customer:
 
         for rental in self.rentals:
             # compute the frequent renter points based on movie price code
-            if rental.get_movie().get_price_code() == Movie.NEW_RELEASE:
-                # New release earns 1 point per day rented
-                frequent_renter_points += rental.get_days_rented()
-            else:
-                # Other rentals get only 1 point
-                frequent_renter_points += 1
+            frequent_renter_points += rental.rental_points()
             #  add a detail line to statement
             statement += rental_fmt.format(
                             rental.get_movie().get_title(),
                             rental.get_days_rented(),
                             rental.get_price())
-            # and accumulate activity
-            total_amount += rental.get_price()
-
         # footer: summary of charges
         statement += "\n"
         statement += "{:40s}  {:6s} {:6.2f}\n".format(
@@ -64,3 +56,6 @@ class Customer:
         statement += "Frequent Renter Points earned: {}\n".format(frequent_renter_points)
 
         return statement
+
+    def total_amount(self):
+        return sum(rental.get_price() for rental in self.rentals)
