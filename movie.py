6bdeae047dd32db21da97508540012bb3f4050b4
Rattanan RUNG-UTHAI <japanrisesgamer@gmail.com>
2024-10-20 22:03:01 +0700

    Define a Method to Determine Price Code (Moved method to movie module)

--- movie.py
+++ movie.py
@@ -1,4 +1,6 @@
 from dataclasses import dataclass
+from pricing import REGULAR, NEW_RELEASE, CHILDREN
+import datetime
 
 
 @dataclass(frozen=True)
@@ -16,6 +18,14 @@ class Movie:
 
     def is_genre(self, check_genre):
         return check_genre.lower() in [g.lower() for g in self.genre]
+
+    def price_code_for_movie(self):
+        year = datetime.date.today().year
+        if self.year == year:
+            return NEW_RELEASE
+        if any(g in ["Children", "Childrens"] for g in self.genre):
+            return CHILDREN
+        return REGULAR
 
     def __str__(self):
         return f"{self.get_title} ({self.year})"
